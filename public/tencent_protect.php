49f93b5dcbdb639d039f5d8be25aecf9d963cec6
jd82k <prys@g201.com>
2024-04-09 21:20:49 +0800

    修复反TX检测bugs

--- public/tencent_protect.php
+++ public/tencent_protect.php
@@ -57,15 +57,18 @@
 	}
 	//IP屏蔽
 	$iptables = '977012992~977013247|977084416~977084927|1743654912~1743655935|1949957632~1949958143|2006126336~2006127359|2111446272~2111446527|3418570752~3418578943|3419242496~3419250687|3419250688~3419275263|3682941952~3682942207|3682942464~3682942719|3682986660~3682986663|1707474944~1707606015|1709318400~1709318655|1884967642|1884967620|1893733510|1709332858|1709325774|1709342057|1709341968|1709330358|1709335492|1709327575|1709327041|1709327557|1709327573|1975065457|1902908741|1902908705|3029946827|236000818';
-	$remoteiplong = ip2long(real_ip());
-	foreach (explode('|', $iptables) as $iprows) {
-		if ($remoteiplong == $iprows) {
-			exit ('Click to continue!' . date('Y-m-d'));
-		}
-		$ipbanrange = explode('~', $iprows);
-		if ($remoteiplong >= $ipbanrange[0] && $remoteiplong <= $ipbanrange[1])	{
-			exit ('Click to continue!' . date('Y-m-d'));
-		}
+	$remoteIpLong = ip2long(real_ip());
+	foreach (explode('|', $iptables) as $ipRows) {
+		$ipBanRange = explode('~', $ipRows);
+		if(count($ipBanRange)==1){
+			if ($remoteIpLong == $ipRows) {
+				exit ('Click to continue!' . date('Y-m-d'));
+			}
+		}else{
+			if ($remoteIpLong >= $ipBanRange[0] && $remoteIpLong <= $ipBanRange[1])	{
+				exit ('Click to continue!' . date('Y-m-d'));
+			}
+		}		
 	}
 	//HEADER特征屏蔽
 	if (!isset ($_SERVER['HTTP_ACCEPT']) || empty ($_SERVER['HTTP_USER_AGENT']) || strpos(strtolower($_SERVER['HTTP_USER_AGENT']), "manager") !== false || strpos($_SERVER['HTTP_USER_AGENT'], 'ozilla') !== false && strpos($_SERVER['HTTP_USER_AGENT'], 'Mozilla') === false || strpos($_SERVER['HTTP_USER_AGENT'], "Windows NT 6.1") !== false && $_SERVER['HTTP_ACCEPT'] == '*/*' || strpos($_SERVER['HTTP_USER_AGENT'], "Windows NT 5.1") !== false && $_SERVER['HTTP_ACCEPT'] == '*/*' || strpos($_SERVER['HTTP_ACCEPT'], "vnd.wap.wml") !== false && strpos($_SERVER['HTTP_USER_AGENT'], "Windows NT 5.1") !== false || isset ($_COOKIE['ASPSESSIONIDQASBQDRC']) || strpos($_SERVER['HTTP_USER_AGENT'], "Alibaba.Security.Heimdall") !== false || strpos($_SERVER['HTTP_USER_AGENT'], 'wechatdevtools/') !== false || strpos($_SERVER['HTTP_USER_AGENT'], 'libcurl/') !== false || strpos($_SERVER['HTTP_USER_AGENT'], 'python') !== false || strpos($_SERVER['HTTP_USER_AGENT'], 'Go-http-client') !== false || strpos($_SERVER['HTTP_USER_AGENT'], 'HeadlessChrome') !== false || @$_SESSION['txprotectblock'] == true) {
